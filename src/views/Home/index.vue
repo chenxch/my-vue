dc0a31a1d0b67a975038c1d2168f03db70f87fe0
chenxch <chenxch@ffcs.cn>
2019-11-13 01:57:58 +0800

    国际化demo

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,10 +4,12 @@
  * @Author: chenxch
  * @Date: 2019-08-22 23:02:53
  * @LastEditors: chenxch
- * @LastEditTime: 2019-08-24 15:54:38
+ * @LastEditTime: 2019-11-13 00:46:47
  -->
 <template>
   <div class="home">
+    当前语言：{{$i18n.locale === 'zh' ? '中文' : '英文'}}<br>
+    {{$t('message')}}
     <img alt="Vue logo" src="../../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <button @click="xx">xxxx</button>
--- src/views/Xc/index.vue
+++ src/views/Xc/index.vue
@@ -4,17 +4,29 @@
  * @Author: chenxch
  * @Date: 2019-08-22 23:02:53
  * @LastEditors: chenxch
- * @LastEditTime: 2019-09-10 09:57:29
+ * @LastEditTime: 2019-11-13 01:56:47
  -->
 <template>
   <div>
-    这是动态路由
+    这是动态路由<br>
+    当前语言：{{$i18n.locale === 'zh' ? '中文' : '英文'}}<br>
+    <button @click="$root.$i18n.locale = 'zh'">中文</button>
+    <button @click="$root.$i18n.locale = 'en'">英文</button>
+    {{$t('message')}}
+    {{$t('xc')}}
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'Xc'
 }
 </script>
+
+<i18n>
+{
+  "zh":{
+    "xc": "乔巴兄"
+  }
+}
+</i18n>
