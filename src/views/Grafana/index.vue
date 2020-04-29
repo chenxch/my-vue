7ed45c9e0d493f28747be0e912fa76e2005169bc
chenxch <chenxch@ffcs.cn>
2020-04-30 01:41:36 +0800

    测试iframe拦截键盘事件

--- src/views/Grafana/index.vue
+++ src/views/Grafana/index.vue
@@ -9,7 +9,7 @@
 <template>
   <div>
     <iframe src='http://121.196.204.61:3000/d/EgbXS25Zk/xc-dashboard?orgId=1&from=1569806402035&to=1569828002035&kiosk'
-     width="100%" height="700" frameborder="0"></iframe>
+     width="100%" height="700" frameborder="0"  @load="load" ref="xc" id="xc"></iframe>
   </div>
 </template>
 
@@ -25,6 +25,22 @@ export default {
   methods: {
     display(e) {
       console.log(e.querySelector('.sidemenu'));
+    },
+    esc(e) {
+      // 此处填写你的业务逻辑即可
+      if (e.keyCode === 27) {
+        console.log(123);
+        // self.closeScreenFull();
+      }
+    },
+    load() {
+      this.$refs.xc.addEventListener('keyup', this.esc);
+      // document.getElementById('xc').contentWindow.addEventListener('keyup', this.esc);
+      // document.getElementById('xc').contentWindow.addEventListener('keydown', this.esc);
+      // document.getElementById('xc').contentWindow.onkeyup = this.esc;
+      document.getElementById('xc').contentWindow.onkeydown = this.esc;
+      // document.getElementById('xc').addEventListener('click', this.esc);
+      window.addEventListener('keydown', this.esc);
     }
   },
   mounted() {
@@ -33,6 +49,15 @@ export default {
     //   console.log(e.target.querySelector('sidemenu'));
     // }
     // // console.log(this.$refs.xc.onload);
+    console.log(document.getElementById('xc'));
+
+    // .onload(() => {
+    //   this.$refs.xc.addEventListener('keyup', this.esc);
+    // });
+    // console.log(this.$refs.xc);
+  },
+  destroyed() {
+    document.removeEventListener('keyup', this.esc);
   }
 };
 </script>
